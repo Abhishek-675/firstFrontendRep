db7a4db8d2684bfa07e9e491b6883ae0cda3cd58
Abhishek-675 <dekaa331@gmail.com>
2022-02-02 15:54:34 +0530

    task 7

--- dom.js
+++ dom.js
@@ -41,10 +41,61 @@
 // var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
 // thirdItem.style.color = 'white';
 
-var secondItem = document.querySelectorAll('li:nth-child(even)');
-console.log(secondItem);
-secondItem[0].style.color = 'green';
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for(var i=0; i<odd.length; i++){
-    odd[i].style.backgroundColor = 'green';
-}
+// var secondItem = document.querySelectorAll('li:nth-child(even)');
+// console.log(secondItem);
+// secondItem[0].style.color = 'green';
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// for(var i=0; i<odd.length; i++){
+//     odd[i].style.backgroundColor = 'green';
+// }
+
+var itemList = document.querySelector('#items');
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// console.log(itemList.childNodes);
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+// console.log(itemList.firstChild);
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+// console.log(itemList.lastChild);
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+// console.log(itemList.nextSibling);
+// console.log(itemList.nextElementSibling);
+
+// console.log(itemList.previousSibling);
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
+
+var newDiv = document.createElement('div');
+newDiv.className = 'hello';
+newDiv.id = 'hello1';
+newDiv.setAttribute('title', 'Hello Div');
+var newDivText = document.createTextNode('HEllo');
+newDiv.appendChild(newDivText);
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+console.log(newDiv);
+// newDiv.style.fontSize = '30px';
+container.insertBefore(newDiv, h1);
+
+var newDiv2 = document.createElement('div');
+newDiv2.className = 'hello2';
+newDiv2.id = 'hello2.1';
+var liText = document.createTextNode('HEllo');
+newDiv2.appendChild(liText);
+var li = document.querySelector('div .list-group');
+var listGroupItem = document.querySelector('ul li');
+console.log(newDiv2);
+li.insertBefore(newDiv2, listGroupItem);
