3e5bc4ca145cfefc65a3e712e035d863fcbf3502
Abhishek-675 <dekaa331@gmail.com>
2022-02-02 13:48:11 +0530

    task 6 1st part

--- dom.js
+++ dom.js
@@ -10,13 +10,13 @@
 // console.log(document.forms);
 
 // console.log(document.getElementById('header-title'));
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
 // console.log(headerTitle);
 // headerTitle.textContent = 'Hello';
 // headerTitle.innerText = 'Goodbye';
 // headerTitle.innerHTML = '<h3>Hello</h3>';
-header.style.borderBottom = 'solid 3px #000';
+// header.style.borderBottom = 'solid 3px #000';
 
 // var item = document.getElementsByClassName('title');
 // console.log(item);
@@ -30,8 +30,13 @@ header.style.borderBottom = 'solid 3px #000';
 // for(var i=0; i<listGroupItem.length; i++){
 //     listGroupItem[i].style.fontWeight = 'bold';
 // }
-var li = document.getElementsByTagName('li');
-console.log(li);
-for(var i=0; i<li.length; i++){
-        li[i].style.color = 'red';
-    }
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// for(var i=0; i<li.length; i++){
+//         li[i].style.color = 'red';
+//     }
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.color = 'white';
