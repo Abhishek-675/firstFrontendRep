e5acf9873e511ed5c7985e2a82ceb170f1d83983
Abhishek-675 <dekaa331@gmail.com>
2022-02-02 14:05:14 +0530

    task 6 2nd part

--- dom.js
+++ dom.js
@@ -36,7 +36,15 @@
 //         li[i].style.color = 'red';
 //     }
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor = 'green';
-var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.color = 'white';
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = 'green';
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.color = 'white';
+
+var secondItem = document.querySelectorAll('li:nth-child(even)');
+console.log(secondItem);
+secondItem[0].style.color = 'green';
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = 'green';
+}
