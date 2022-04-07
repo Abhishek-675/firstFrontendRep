802a739f372a51501f3b8090d27f8aaf2d72011d
Abhishek-675 <dekaa331@gmail.com>
2022-04-07 22:29:38 +0530

    task 2 added

--- Node js sharpener/first-app.js
+++ Node js sharpener/first-app.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+// console.log('Hello World');
+
+const product = (a, b) => {
+    return console.log(a*b);
+};
+
+product(2, 2);
+
+const student = {
+    name: 'Bob',
+    age: 25,
+    greet() {
+        console.log('My name is ' + this.name + ' and age = ' + this.age);
+    }
+}
+
+student.greet();
